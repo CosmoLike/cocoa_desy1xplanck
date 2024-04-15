eea1ed54e991018ba4510deb813eedcd26f38c10
JiachuanXu <jiachuanxu@email.arizona.edu>
2024-04-15 16:21:10 -0700

    move testing functions to test_baryon.cpp

--- interface/test_baryon.cpp
+++ interface/test_baryon.cpp
@@ -12,20 +12,40 @@
 #include "cosmolike/redshift_spline.h"
 #include "cosmolike/structs.h"
 
+void print_SPk(const char* sim_name, int sim_id){
+  // Test dataset reading
+  std::ostringstream output_fn;
+  output_fn << "test_Baryon_read_" << sim_name << "_" << sim_id << "_HDF5C.txt";
+  std::ofstream output(output_fn);
+  if (!output){
+    std::cerr << "Error opening file." << std::endl;
+    exit(1);
+  }
+  output << "# z lgk lgPkR" << std::endl;
+  for (int i=0; i<bary.Na_bins; i++){
+    for (int j=0; j<bary.Nk_bins; j++){
+      output << 1.0/bary.a_bins[i]-1.0 << " " << bary.logk_bins[j] << " " << bary.log_PkR[j*bary.Na_bins+i] << std::endl;
+    }
+  }
+  output.close();
+}
 
 // Test HDF5 reading routine
 int main(){
-  init_baryons("TNG100", 1);
-  init_baryons("HzAGN", 1);
-  init_baryons("mb2", 1);
-  init_baryons("illustris", 1);
-  init_baryons("eagle", 1);
-  init_baryons("cowls_AGN", 1);
-  init_baryons("cowls_AGN", 2);
-  init_baryons("cowls_AGN", 3);
-  init_baryons("BAHAMAS", 1);
-  init_baryons("BAHAMAS", 2);
-  init_baryons("BAHAMAS", 3);
-  for (int i = 1; i< 401; i++) init_baryons("antilles", i);
+  init_baryons("TNG100", 1);print_SPk("TNG100", 1);
+  init_baryons("HzAGN", 1);print_SPk("HzAGN", 1);
+  init_baryons("mb2", 1);print_SPk("mb2", 1);
+  init_baryons("illustris", 1);print_SPk("illustris", 1);
+  init_baryons("eagle", 1);print_SPk("eagle", 1);
+  init_baryons("cowls_AGN", 1);print_SPk("cowls_AGN", 1);
+  init_baryons("cowls_AGN", 2);print_SPk("cowls_AGN", 2);
+  init_baryons("cowls_AGN", 3);print_SPk("cowls_AGN", 3);
+  init_baryons("BAHAMAS", 1);print_SPk("BAHAMAS", 1);
+  init_baryons("BAHAMAS", 2);print_SPk("BAHAMAS", 2);
+  init_baryons("BAHAMAS", 3);print_SPk("BAHAMAS", 3);
+  for (int i = 1; i< 401; i++){
+    init_baryons("antilles", i);
+    print_SPk("antilles", i);
+  }
   std::cout << "test_baryon: Done!" << std::endl;
 }
