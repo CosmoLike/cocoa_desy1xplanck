ee72489739acc7d88f347add7cc56b88545bfb71
JiachuanXu <jiachuanxu@email.arizona.edu>
2024-05-13 14:40:43 -0700

    u

--- emulator/train_emulator.py
+++ emulator/train_emulator.py
@@ -283,7 +283,7 @@ with Pool() as pool:
     sampler.run_mcmc(pos0, config.n_mcmc, progress=True)
     # save the sampler for debug purpose
     if (args.debug):
-        np.save(pjoin(config.traindir, f'DBG_chain_{n}.npy'), 
+        np.save(pjoin(config.traindir, f'DBG_nn{config.nn_model}_chain_{n}.npy'), 
             np.concatenate([sampler.get_chain(), 
                 sampler.get_log_prob()[:,:,np.newaxis]], axis=2))
 
@@ -293,8 +293,8 @@ if(args.temper):
     # only save samples to explore posterior regions
     select_indices = np.random.choice(np.arange(len(samples)), replace=False, size=config.n_resample)
     next_training_samples = samples[select_indices,:-(config.n_fast_pars)]
-    np.save(pjoin(config.traindir, f'samples_{n+1}.npy'), next_training_samples)
+    np.save(pjoin(config.traindir, f'samples_nn{config.nn_model}_{n+1}.npy'), next_training_samples)
 else:
     # we want the chain
-    np.save(pjoin(config.chaindir, config.chainname+f'_{n}.npy'), samples)
+    np.save(pjoin(config.chaindir, config.chainname+f'_nn{config.nn_model}_{n}.npy'), samples)
 print("train_emulator.py: iteration %d Done!"%n)
